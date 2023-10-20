05c5c71be3302e9d370bc5aec840c9a414894440
albertomenache <albertomenache@users.noreply.github.com>
2023-10-20 14:01:09 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PhotoClean
+
 Cleaner for photos with sensor line artifacts
+
+![Screenshot_1](/icons/Screenshot1.png?raw=true "App Screenshot")
+
+Parts of the UI are based on a modified version of https://github.com/ap193uee/PyQt-Image-Viewer
---
+++ main.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+''' A basic GUi to use ImageViewer class to show its functionalities and use cases. '''
+
+from PyQt5 import QtCore, QtWidgets, uic, QtWidgets
+from actions import ImageViewer
+import sys, os
+import style
+
+gui = uic.loadUiType("main.ui")[0]     # load UI file designed in Qt Designer
+VALID_FORMAT = ('.BMP', '.GIF', '.JPG', '.JPEG', '.PNG', '.PBM', '.PGM', '.PPM', '.TIFF', '.XBM')  # Image formats supported by Qt
+
+def getImages(folder):
+    ''' Get the names and paths of all the images in a directory. '''
+    image_list = []
+    if os.path.isdir(folder):
+        for file in os.listdir(folder):
+            if file.upper().endswith(VALID_FORMAT):
+                im_path = os.path.join(folder, file)
+                image_obj = {'name': file, 'path': im_path }
+                image_list.append(image_obj)
+    return image_list
+
+class Iwindow(QtWidgets.QMainWindow, gui):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+        self.cntr, self.numImages = -1, -1  # self.cntr have the info of which image is selected/displayed
+
+        self.image_viewer = ImageViewer(self.qlabel_image)
+        self.result_viewer = ImageViewer(self.qlabel_result)
+        self.__connectEvents()
+        self.showMaximized()
+
+    def __connectEvents(self):
+        self.open_folder.clicked.connect(self.selectDir)
+        self.next_im.clicked.connect(self.nextImg)
+        self.prev_im.clicked.connect(self.prevImg)
+        self.qlist_images.itemClicked.connect(self.item_click)
+
+    def selectDir(self):
+        ''' Select a directory, make list of images in it and display the first image in the list. '''
+        # open 'select folder' dialog box
+        self.folder = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory"))
+        if not self.folder:
+            QtWidgets.QMessageBox.warning(self, 'No Folder Selected', 'Please select a valid Folder')
+            return
+        
+        self.logs = getImages(self.folder)
+        self.numImages = len(self.logs)
+
+        # make qitems of the image names
+        self.items = [QtWidgets.QListWidgetItem(log['name']) for log in self.logs]
+        for item in self.items:
+            self.qlist_images.addItem(item)
+
+        # display first image and enable Pan 
+        self.cntr = 0
+        self.image_viewer.enablePan(False)
+        self.result_viewer.enablePan(False)
+        result = self.image_viewer.loadImage(self.logs[self.cntr]['path'])
+        self.items[self.cntr].setSelected(True)
+        self.result_viewer.showImage(result)
+        #self.qlist_images.setItemSelected(self.items[self.cntr], True)
+
+        # enable the next image button on the gui if multiple images are loaded
+        if self.numImages > 1:
+            self.next_im.setEnabled(True)
+
+    def resizeEvent(self, evt):
+        if self.cntr >= 0:
+            self.image_viewer.onResize()
+            self.result_viewer.onResize()
+
+    def nextImg(self):
+        if self.cntr < self.numImages -1:
+            self.cntr += 1
+            result = self.image_viewer.loadImage(self.logs[self.cntr]['path'])
+            self.items[self.cntr].setSelected(True)
+            self.result_viewer.showImage(result)
+            #self.qlist_images.setItemSelected(self.items[self.cntr], True)
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Sorry', 'No more Images!')
+
+    def prevImg(self):
+        if self.cntr > 0:
+            self.cntr -= 1
+            result = self.image_viewer.loadImage(self.logs[self.cntr]['path'])
+            self.items[self.cntr].setSelected(True)
+            self.result_viewer.showImage(result)
+            #self.qlist_images.setItemSelected(self.items[self.cntr], True)
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Sorry', 'No previous Image!')
+
+    def item_click(self, item):
+        self.cntr = self.items.index(item)
+        result = self.image_viewer.loadImage(self.logs[self.cntr]['path'])
+        self.result_viewer.showImage(result)
+        
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    app.setStyleSheet(style.style)
+    app.setPalette(QtWidgets.QApplication.style().standardPalette())
+    parentWindow = Iwindow(None)
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    #print __doc__
+    main()
